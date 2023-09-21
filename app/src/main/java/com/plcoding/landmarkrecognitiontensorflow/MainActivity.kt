5c29e4c0d8a803045989b7ec33da10dfc15a95f3
Philipp Lackner <mail@pl-coding.com>
2023-09-21 15:29:50 +0200

    Initial commit

---
+++ app/src/main/java/com/plcoding/landmarkrecognitiontensorflow/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.plcoding.landmarkrecognitiontensorflow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.landmarkrecognitiontensorflow.ui.theme.LandmarkRecognitionTensorflowTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LandmarkRecognitionTensorflowTheme {
+
+            }
+        }
+    }
+}
